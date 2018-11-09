70a78fe3696e8d43196b16765377594393c38abb
aleksi <aleksijonathanhamalainen@gmail.com>
2018-11-09 10:01:17 +0200

    Trajectory generation updated.

--- scripts/generate_trajectories.py
+++ scripts/generate_trajectories.py
@@ -1,25 +1,36 @@
 #! /usr/bin/env python
-from motion_planning.controller import RobotScene, RobotWrapper
+from motion_planning.simulation_interface import SimulationInterface
 from motion_planning.trajectory_parser import TrajectoryParser
-from motion_planning.utils import KUKA_X_LIM, KUKA_Y_LIM, KUKA_z_LIM, parse_arguments, KUKA_RESET_JOINTS
+import argparse
+import os
 
-args = parse_arguments()
 
-SAVE_PATH = args.save_path
-SAVE_FILE = args.save_file
+parser = argparse.ArgumentParser(description='Trajectory Generator Arguments')
+parser.add_argument('--save-folder', default='example', type=str, help='')
+parser.add_argument('--num_samples', default=20000, type=int, help='Number of samples')
+parser.add_argument('--num_joints', default=7, type=int, help='')
+
+args = parser.parse_args()
+
+ROOT_PATH = '../trajectory_data'
+SAVE_PATH = os.path.join(ROOT_PATH, args.save_folder)
+
 NUM_SAMPLES = args.num_samples
 NUM_JOINTS = args.num_joints
 
+
 if __name__ == '__main__':
 
-        robot = RobotWrapper('full_lwr', KUKA_RESET_JOINTS)
-        env = RobotScene(robot, KUKA_X_LIM, KUKA_Y_LIM, KUKA_z_LIM)
-        parser = TrajectoryParser(SAVE_PATH, SAVE_FILE, NUM_JOINTS)
+    gripper_name = None
+    simulation_interface = SimulationInterface('arm', gripper_name)
+    trajectory_saver = TrajectoryParser(SAVE_PATH, 'trajectories', NUM_JOINTS)
 
-        for i in range(NUM_SAMPLES):
-                plan = env.random_plan()
-                parser.add_trajectory(plan)
+    for i in range(NUM_SAMPLES):
+        plan = simulation_interface.random_plan()
+        trajectory_saver.add_trajectory(plan)
 
-                if i % 200 == 0:
-                        parser.save()
-                        print(i)
+        if i % 200 == 0:
+            trajectory_saver.save()
+            print('Generated and Saved: '.format(i))
+
+    trajectory_saver.save()
--- src/motion_planning/trajectory_parser.py
+++ src/motion_planning/trajectory_parser.py
@@ -4,36 +4,40 @@ import pickle
 
 def parse_trajectory(trajectory):
 
-        trajectory = trajectory.joint_trajectory.points
+    trajectory = trajectory.joint_trajectory.points
 
-        time_steps_raw = np.array([motion.time_from_start.to_sec() for motion in trajectory])
-        positions_raw = np.stack(np.array(motion.positions) for motion in trajectory)
-        velocities_raw = np.stack(np.array(motion.velocities) for motion in trajectory)
-        accelerations_raw = np.stack(np.array(motion.accelerations) for motion in trajectory)
+    time_steps_raw = np.array([motion.time_from_start.to_sec() for motion in trajectory])
+    positions_raw = np.stack(np.array(motion.positions) for motion in trajectory)
+    velocities_raw = np.stack(np.array(motion.velocities) for motion in trajectory)
+    accelerations_raw = np.stack(np.array(motion.accelerations) for motion in trajectory)
 
-        return time_steps_raw, positions_raw, velocities_raw, accelerations_raw
+    return time_steps_raw, positions_raw, velocities_raw, accelerations_raw
 
 class TrajectoryParser(object):
 
-        def __init__(self, save_path, save_file, num_joints):
+    def __init__(self, save_path, save_file, num_joints):
 
-                self.save_path = save_path
-                self.save_file = save_file
-                self.num_joints = num_joints
-                self.time_steps_raw = []
-                self.positions_raw = []
-                self.velocities_raw = []
-                self.accelerations_raw = []
+        self.save_path = save_path
 
-        def add_trajectory(self, trajectory):
+        if not(os.path.exists(self.save_path)):
+            os.makedirs(self.save_path)
 
-                time_steps_raw, positions_raw, velocities_raw, accelerations_raw = parse_trajectory(trajectory)
+        self.save_file = save_file
+        self.num_joints = num_joints
+        self.time_steps_raw = []
+        self.positions_raw = []
+        self.velocities_raw = []
+        self.accelerations_raw = []
 
-                self.time_steps_raw.append(time_steps_raw)
-                self.positions_raw.append(positions_raw)
-                self.velocities_raw.append(velocities_raw)
-                self.accelerations_raw.append(accelerations_raw)
+    def add_trajectory(self, trajectory):
 
-        def save(self):
-                pickle.dump([np.array(self.time_steps_raw), np.array(self.positions_raw), np.array(self.velocities_raw), np.array(self.accelerations_raw)],
-                            open(os.path.join(self.save_path, '{}.pkl'.format(self.save_file)), 'wb'))
+        time_steps_raw, positions_raw, velocities_raw, accelerations_raw = parse_trajectory(trajectory)
+
+        self.time_steps_raw.append(time_steps_raw)
+        self.positions_raw.append(positions_raw)
+        self.velocities_raw.append(velocities_raw)
+        self.accelerations_raw.append(accelerations_raw)
+
+    def save(self):
+        pickle.dump([np.array(self.time_steps_raw), np.array(self.positions_raw), np.array(self.velocities_raw), np.array(self.accelerations_raw)],
+                    open(os.path.join(self.save_path, '{}.pkl'.format(self.save_file)), 'wb'))
