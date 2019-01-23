47916e42e315c9f0a570152e2c635f9a398098ba
aleksi <aleksijonathanhamalainen@gmail.com>
2019-01-23 14:32:00 +0200

    behavioural vae included

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-        packages=['motion_planning', 'behavioural_layer', 'gibson', 'gibson.models'],
+        packages=['motion_planning', 'behavioural_vae', 'gibson', 'gibson.models'],
         package_dir={'': 'src'})
 
 setup(**setup_args)
