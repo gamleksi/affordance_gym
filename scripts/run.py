6d0450918aa312e47d6588f9c3cf31ede33804c8
Aleksi <aleksi.hamalainen@aalto.fi>
2019-01-28 11:15:25 +0200

    run.py updated

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 from behavioural_vae.ros_monitor import ROSTrajectoryVAE, RosTrajectoryConvVAE
-from behavioural_vae.visual import TrajectoryVisualizer
 from behavioural_vae.utils import smooth_trajectory, MIN_ANGLE, MAX_ANGLE
 from motion_planning.simulation_interface import SimulationInterface
 from motion_planning.monitor import TrajectoryEnv
@@ -11,6 +10,24 @@ import pickle
 from tqdm import tqdm
 
 
+DATASET_PATH = "/home/aleksi/catkin_ws/src/motion_planning/src/behavioural_vae/dset/new_base_rtt_star"
+
+
+def load_data(file_path, num_joints, num_actions):
+
+    time_steps_raw, positions_raw, _, _, end_poses = np.load(os.path.join(DATASET_PATH, 'trajectories.pkl'))
+    num_samples = len(time_steps_raw)
+
+    positions = np.zeros([num_samples, num_joints, num_actions])
+
+    for i in range(num_samples):
+        _, smooth_positions, _, _ = smooth_trajectory(time_steps_raw[i], positions_raw[i], num_actions, num_joints)
+        positions[i] = smooth_positions
+    positions = (positions - MIN_ANGLE) / (MAX_ANGLE - MIN_ANGLE)
+
+    return positions, end_poses
+
+
 def main(args):
 
     model_name = args.vae_name
@@ -19,30 +36,31 @@ def main(args):
     num_actions = args.num_actions
     num_joints = args.num_joints
 
-    models_folder = os.path.join(BEHAVIOUR_ROOT, 'log', model_name, 'models')
+    models_folder = os.path.join(BEHAVIOUR_ROOT, model_name)
+    print(models_folder)
 
     assert(os.path.exists(models_folder))
 
     listdir = os.listdir(models_folder)
 
-    num_models = len(listdir)
+    num_models = 10
 
     simulation_interface = SimulationInterface('lumi_arm')
 
-    save_path = os.path.join(BEHAVIOUR_ROOT, 'log', model_name, 'reconstruction_results')
+    save_path = os.path.join(BEHAVIOUR_ROOT, model_name, 'reconstruction_results')
 
     if (not(os.path.exists(save_path))):
         os.makedirs(save_path)
 
-    trajectories, end_poses = load_data(args.dataset_name, args.num_joints, args.num_actions)
+    trajectories, end_poses = load_data("hello", args.num_joints, args.num_actions)
 
-    for model_idx in range(num_models):
+    for model_idx in range(4, num_models):
 
         if args.conv:
             behaviour_model = RosTrajectoryConvVAE(model_name, latent_dim, num_actions, args.kernel_row, args.conv_channel)
         else:
 
-            behaviour_model = ROSTrajectoryVAE(model_name, latent_dim, num_actions, model_index=model_idx, num_joints=num_joints,  root_path=BEHAVIOUR_ROOT)
+            behaviour_model = ROSTrajectoryVAE(os.path.join(BEHAVIOUR_ROOT, model_name), latent_dim, num_actions, model_index=model_idx, num_joints=num_joints)
 
         env = TrajectoryEnv(behaviour_model, simulation_interface, num_actions, num_joints=num_joints, trajectory_duration=duration)
         env.reset_environment(3.0)
@@ -65,10 +83,6 @@ def main(args):
             latents.append(latent)
             recons.append(recon)
 
-            # TODO Visualization of end poses to get knowledge of the neigborhood properties
-            # TODO MSE end poses to get knowledge of the reconstruction accuracy and distribution
-            # TODO Policy debugging network!
-
         pose_results = np.array(pose_results)
         latents = np.array(latents)
         print("norm", np.linalg.norm(end_poses - pose_results))
@@ -82,20 +96,6 @@ def main(args):
 
         f.close()
 
-
-def load_data(file_path, num_joints, num_actions):
-
-    time_steps_raw, positions_raw, _, _, end_poses = np.load(os.path.join("../trajectory_data", file_path, 'trajectories.pkl'))
-    num_samples = len(time_steps_raw)
-
-    positions = np.zeros([num_samples, num_joints, num_actions])
-
-    for i in range(num_samples):
-        _, smooth_positions, _, _ = smooth_trajectory(time_steps_raw[i], positions_raw[i], num_actions, num_joints)
-        positions[i] = smooth_positions
-    positions = (positions - MIN_ANGLE) / (MAX_ANGLE - MIN_ANGLE)
-
-    return positions, end_poses
 
 if __name__ == '__main__':
 
